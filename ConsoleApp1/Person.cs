c2249c75998bcd4204103f976197e7b3003f33b6
Tristan Wunsch <wunsch.t03@htlwienwest.at>
2021-01-08 14:16:27 +0100

    Class "Person" added

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public string Firstname { get; }
+        public string Lastname { get; }
+        public Person(string fn, string ln)
+        {
+            Firstname = fn;
+            Lastname = ln;
+        }
+    }
+}
